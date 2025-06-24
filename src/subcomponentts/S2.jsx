a5fb8555a4bb04c22fa7d00e26614202e6ec9220
vansh7041 <barotvansh1410@gmail.com>
2025-06-24 17:24:10 +0530

    your update

--- src/subcomponentts/S2.jsx
+++ src/subcomponentts/S2.jsx
@@ -2,26 +2,19 @@ import React from 'react'
 
 const S2 = () => {
   return (
-   <>
-    <div className='text-white font-bold   z-50 absolute'>
- <h1 className='text-black font-extrabold text-3xl'>Food Cafe website</h1>
+    <>
+      <div className="text-white font-bold  max-w-4xl mx-auto">
+        <h1 className="text-black font-extrabold text-3xl mb-4 text-center sm:text-left">
+          Clone Website
+        </h1>
 
-   <p className='mt-4'>This is a multi-section website designed for a fictional food café. It is developed using HTML, CSS, and JavaScript, showcasing a modern layout . The homepage features an <br />image slider to highlight featured dishes or offers, and the <br /> site includes a contact form for customer inquiries. <br />
-
-Key Features: <br />
-
-1. Eye-catching hero slider using vanilla JavaScript  <br />
-
-2. Responsive layout for mobile, tablet, and desktop <br />
-
-3. Contact form with validation (HTML5 + JS) <br />
-
-
-
- </p>
-   </div>
-  
-   </>
+        <p className="text-white text-base sm:text-lg leading-relaxed">
+          This is a multi-section website designed for a fictional food café. It is developed using HTML, CSS, and JavaScript, showcasing a modern layout . The homepage features an image slider to highlight featured dishes or offers, and the site includes a contact form for customer inquiries.
+          <br /><br />
+          
+        </p>
+      </div>
+    </>
   )
 }
 
